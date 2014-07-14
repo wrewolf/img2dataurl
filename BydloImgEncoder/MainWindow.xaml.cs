e71b5c5f7278ccdf6f34efc1717193ea7fb55b8a
wrewolf <wrewolf@gmail.com>
2014-07-14 10:59:25 +0400

    init

---
+++ BydloImgEncoder/MainWindow.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace BydloImgEncoder
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            // Configure open file dialog box
+            Microsoft.Win32.OpenFileDialog dlg = new Microsoft.Win32.OpenFileDialog();
+            dlg.FileName = "Image"; // Default file name
+            dlg.DefaultExt = ".png"; // Default file extension
+            dlg.Filter = "Image file (.png)|*.jpg"; // Filter files by extension 
+
+            // Show open file dialog box
+            Nullable<bool> result = dlg.ShowDialog();
+
+            // Process open file dialog box results 
+            if (result == true)
+            {
+                // Open document 
+                string filename = dlg.FileName;
+                PathField.Content = dlg.FileName;
+                encoded.Text= "<img src=\"data:image/"
+                        + System.IO.Path.GetExtension(filename).Replace(".", "")
+                        + ";base64,"
+                        + Convert.ToBase64String(File.ReadAllBytes(filename)) + "\" />";
+
+            }
+        }
+
+        private void MouseDownBase64(object sender, MouseButtonEventArgs e)
+        {
+            Clipboard.SetDataObject(encoded.Text);
+        }
+    }
+}
